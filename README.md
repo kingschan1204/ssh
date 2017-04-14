b1922275333e90377a6379d3f248d371054c09cd
kings.chan <kings.chan@qq.com>
2017-04-14 18:00:51 +0800

    add demo table

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ssh
-springMvc+spring+hibernate 搭建
+springMvc+spring+hibernate ssh搭建
+
+## 表结构
+```
+CREATE TABLE `ssh-users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `sex` bit(1) NOT NULL,
+  `age` tinyint(4) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `birthday` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `remark` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+```
---
+++ src/main/java/com/github/kingschan1204/ssh/model/po/SshUsersEntity.java
@@ -0,0 +1,132 @@
+package com.github.kingschan1204.ssh.model.po;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by kingschan on 2017/4/14.
+ */
+@Entity
+@Table(name = "ssh-users")
+public class SshUsersEntity {
+    private int id;
+    private String username;
+    private String password;
+    private boolean sex;
+    private byte age;
+    private String email;
+    private Timestamp birthday;
+    private String remark;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "sex")
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    @Basic
+    @Column(name = "age")
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "birthday")
+    public Timestamp getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Timestamp birthday) {
+        this.birthday = birthday;
+    }
+
+    @Basic
+    @Column(name = "remark")
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SshUsersEntity that = (SshUsersEntity) o;
+
+        if (id != that.id) return false;
+        if (sex != that.sex) return false;
+        if (age != that.age) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (birthday != null ? !birthday.equals(that.birthday) : that.birthday != null) return false;
+        if (remark != null ? !remark.equals(that.remark) : that.remark != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (sex ? 1 : 0);
+        result = 31 * result + (int) age;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        result = 31 * result + (remark != null ? remark.hashCode() : 0);
+        return result;
+    }
+}
